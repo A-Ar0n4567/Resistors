a3b6b89a13ca51eb23d9f724c23071614f39bdd2
aamir7110 <aamir7110@7L9N6Y1.Student.UGDSB.ED>
2020-02-27 13:57:44 -0500

    Resistors First Commit

---
+++ src/resistors/Resistors.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package resistors;
+
+import javax.swing.*;
+/**
+ *
+ * @author aamir7110
+ */
+public class Resistors {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String color[] = {"Black","Brown","red","Orange","Yellow","Green","Blue","Violet","Grey","White"};
+        
+    }
+    
+}
